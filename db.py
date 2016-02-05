c59708db1fd835f767b8db8aca10358f8cb507ef
Sven Hollowell <svenh91@gmail.com>
2016-02-05 17:41:59 +0000

    add/remove participants to studies

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import IntegrityError
 import sys
 import os
 
@@ -12,12 +13,51 @@ def create_cursor():
 		pass
 	cur = conn.cursor()
 
-def get_all_participants():
-	cur.execute("""SELECT participant_id, name From Participants""")
+def get_studies(study_id=None):
+	if study_id is not None:
+		cur.execute("""SELECT study_id, name FROM Studies WHERE study_id=%s""", [study_id])
+	else:
+		cur.execute("""SELECT study_id, name FROM Studies""")
 	return cur.fetchall()
 
-def get_all_users():
-	cur.execute("""SELECT user_id,username,password FROM Users""")
+
+def get_studyparticipants(study_id=None, participant_id=None):
+	if study_id is not None and participant_id is not None:
+		cur.execute("""SELECT * FROM StudyParticipants WHERE (study_id=%s AND participant_id=%s)""", [study_id, participant_id])
+	elif study_id is not None:
+		cur.execute("""SELECT * FROM StudyParticipants WHERE study_id=%s""", [study_id])
+	elif participant_id is not None:
+		cur.execute("""SELECT * FROM StudyParticipants WHERE participant_id=%s""", [participant_id])
+	else:
+		cur.execute("""SELECT * FROM StudyParticipants""")
+	return cur.fetchall()
+
+def add_studyparticipant(study_id, participant_id):
+	try:
+		cur.execute("""INSERT INTO StudyParticipants(study_id, participant_id) VALUES(%s, %s)""", [study_id, participant_id])
+		return True
+	except IntegrityError:
+		print "adding study - participant (%s, %s) link failed: already exists!" % (study_id, participant_id)
+		return False
+def remove_studyparticipant(study_id, participant_id):
+	cur.execute("""DELETE FROM StudyParticipants WHERE (study_id=%s AND participant_id=%s)""", [study_id, participant_id])
+
+def get_participants(participant_id=None, study_id=None):
+	command = """SELECT Participants.participant_id, Participants.name FROM Participants"""
+	if participant_id is not None:
+		command += cur.mogrify(""" WHERE Participants.participant_id=%s""",[participant_id])
+	
+	if study_id is not None:
+		command += cur.mogrify(""" INNER JOIN StudyParticipants ON (Participants.participant_id=StudyParticipants.participant_id AND StudyParticipants.study_id=%s)""",[study_id])
+	print command
+	cur.execute(command)
+	return cur.fetchall()
+
+def get_users(user_id=None):
+	command = """SELECT user_id,username,password FROM Users"""
+	if user_id is not None:
+		command += cur.mogrify(" WHERE user_id=%s", user_id)
+	cur.execute(command)
 	return cur.fetchall()
 
 def get_user_studies(user_id):
@@ -27,18 +67,45 @@ def get_user_studies(user_id):
 	return cur.fetchall()
 
 def add_participant(name):
-	if name in [x[1] for x in get_all_participants()]:
+	if name in [x[1] for x in get_participants()]:
 		print name, "already exsists"
-	cur.execute("""INSERT INTO Participants(participant_id, name) VALUES(default, %s)""", [name])
+	cur.execute("""INSERT INTO Participants(participant_id, name) VALUES(default, %s) RETURNING participant_id""", [name])
+	try:
+		return cur.fetchone()[0]
+	except NameError:
+		return None
 
-def add_image(image_time, user_id, full_url, medium_url, thumbnail_url):
-	cur.execute("""INSERT INTO Images(image_time, user_id, full_url, medium_url, thumbnail_url, ) VALUES(default, %s)""", [])
+def get_images(participant_id=None, only_number=False):
+	if only_number:
+		cur.execute("""SELECT COUNT(*) FROM Images""")
+		return cur.fetchone()[0]
+	else:
+		if participant_id is not None: # check it's an integer
+			cur.execute("""SELECT * FROM Images WHERE participant_id=%s""", [str(participant_id)])
+		else:
+			cur.execute("""SELECT * FROM Images""")
+		return cur.fetchall()
 
-def create_db():
+def add_image(image_time, participant_id, full_url, medium_url, thumbnail_url):
+	cur.execute("""INSERT INTO Images(image_time, participant_id, full_url, medium_url, thumbnail_url) VALUES(%s, %s, %s, %s, %s)""", [image_time, participant_id, full_url, medium_url, thumbnail_url])
+	# conn.commit()
+
+def add_images(image_array):
+	args_str = ','.join(cur.mogrify("(%s,%s,%s,%s,%s)", x) for x in image_array)
+	print "len:", len(args_str)
+	print
+	print args_str[:500]
+	print
+	print args_str[-500:]
+	cur.execute("""INSERT INTO Images(image_time, participant_id, full_url, medium_url, thumbnail_url) VALUES """ + args_str)
+	# conn.commit()
+
+def create_db(and_add_images=False):
 	str = ""
 	with open('create_db.txt','r') as f:
 		txt = f.read()[3:] # remove weird 3  characters at start, bug?
 	for command in txt.split(';'):
+		print command
 		try:
 			cur.execute(command)
 			# print command
@@ -46,24 +113,36 @@ def create_db():
 			print "ProgrammingError\n:Command skipped: ", sys.exc_info(), command
 		except psycopg2.InternalError:
 			print "InternalError\n:Command skipped: ", sys.exc_info(), command
-	conn.commit()
+	# conn.commit()
 
-def add_images():
+	if add_images:
+		images = load_images()
+		print images[0]
+		add_images(images)
+	return txt
+
+def load_images():
+	image_array = []
 	image_folder = os.path.join(script_folder,'images')
 	participants = os.listdir(image_folder)
 	print participants
 	for p in participants:
-		add_participant(p)
-		p_dir = os.path.join(image_folder,p)
-		for img in os.listdir(os.path.join(p_dir,"full")): # only where we have full resolution
-			img_time = parse_img_date(img)
-			(full_img, med_img, thumb_img) = map(lambda x: os.path.join('images',p,x,img), ('full', 'medium', 'thumbnail'))
-			print "\n".join([full_img, med_img, thumb_img])
-			if os.path.isfile(os.path.join(script_folder, full_img)):
-				print "good file"
-				# add_image(img_time, )
-			else:
-				print os.path.join(script_folder, full_img)
+		participant_id = add_participant(p)
+		print "created participant " + str(p) + " id: " + str(participant_id)
+		if participant_id is not None:
+			p_dir = os.path.join(image_folder,p)
+			for img in os.listdir(os.path.join(p_dir,"full")): # only where we have full resolution
+				img_time = parse_img_date(img)
+				(full_img, med_img, thumb_img) = map(lambda x: os.path.join('images',p,x,img), ('full', 'medium', 'thumbnail'))
+				# print "\n".join([full_img, med_img, thumb_img])
+				if os.path.isfile(os.path.join(script_folder, full_img)):
+					# print "good file"
+					image_array.append((img_time, participant_id, full_img, med_img, thumb_img))
+					# add_image(img_time, )
+				else:
+					print os.path.join(script_folder, full_img)
+			add_studyparticipant(4, participant_id)
+	return image_array
 
 def parse_img_date(n):
 	return "".join([
@@ -75,5 +154,7 @@ def parse_img_date(n):
         n[30:32]+".", # seconds
         n[6:9] # this is the photo's sequence number, used as a tiebreaker millisecond value for photos with the same timestamp 
        ])
+	
 conn = psycopg2.connect("dbname='linker' user='postgres' host='localhost' password='testing' port='3145'")
+conn.autocommit = True
 create_cursor()
--- server.py
+++ server.py
@@ -1,33 +1,60 @@
 # import flask
-from flask import Flask
+from flask import Flask, request, redirect
+# import our custom db interfact
 import db
-app = Flask(__name__)
 
-print db.get_all_participants()
-print db.get_all_users()
-res = str(" ".join(map(str,db.get_all_participants())))
-print res
-db.add_images()
+app = Flask(__name__, static_folder="images") # change to non static in future
 
+# db.create_db(and_add_images=True)
 @app.route("/")
 def hello():
 	try:
-		res = "<h1>Participants</h1>\n"
-		for participant in db.get_all_participants():
-			res += " : ".join([str.strip(str(x)) for x in participant]) + "<br>\n"
-		res += "<h1>Users</h1>\n"
-		for user in db.get_all_users():
+		res = "<h2>Control Panel:</h2>\n"
+		# for sp in db.get_studyparticipants():
+		# 	res += " : ".join([str.strip(str(x)) for x in sp]) + "<br>"
+ 
+		res += "<h3>Studies:</h3>\n"
+		for study in db.get_studies():
+			res += "<a href='/study/%s'>" % (study[0],)  
+			res += " : ".join([str.strip(str(x)) for x in study])
+			res += "</a> (" + str(len(db.get_studyparticipants(study_id=study[0]))) + " participants) <br>\n"
+
+		res += "<h3>Participants:</h3>\n"
+		for participant in db.get_participants():
+			res += "<a href='/participant/%s'>" % (participant[0],)  
+			res += " : ".join([str.strip(str(x)) for x in participant])
+			res += "</a> (in " + str(len(db.get_studyparticipants(participant_id=participant[0]))) + " studies)<br>\n"
+
+		res += "<h3>Users:</h3>\n"
+		for user in db.get_users():
 			res += "<a href='/users/%s'>" % (user[0],)  
-			res += " : ".join([str.strip(str(x)) for x in user]) + "<br>\n"
-		res += "<br>\n<br>\n<a href='/create'><button>reboot DB</button></a>"
+			res += " : ".join([str.strip(str(x)) for x in user]) 
+			res += "</a><br>\n"
+		res += "<br>\n<br>\n<a href='/reboot_db'><button>reboot DB</button></a>"
+		n = db.get_images(only_number=True)
+		print n
+		res += "<p>there are " + str(n) + " images in the db</p>"
+		with open('create_db.txt','r') as f:
+			res += "<textarea style='position:absolute; right:3px; top:3px' rows='50' cols='100'>"
+			res += f.read()
+			res += "</textarea>"
 		return res 
 	except Exception as e:
 		return str(e)
 
-@app.route("/create")
+@app.route("/reboot_db")
 def create():
-	db.create_db()
-	return "success"
+	res = db.create_db(and_add_images=True)
+	res = "<h2>Rebooted DB</h2><p>"+ ";</p><p>\n".join(res.split(";")) + "</p"
+	return res
+
+@app.route("/images")
+def images():
+	res = "<h1>All Images</h1>\n"
+	for image in db.get_images():
+		res += "<img src='/%s' href='/%s'>" % (image[5], image[3])
+		res += " : ".join([str.strip(str(x)) for x in image]) + "<br>\n"
+	return res
 
 @app.route("/users/<int:user_id>")
 def user(user_id):
@@ -37,9 +64,63 @@ def user(user_id):
 		res += " : ".join([str.strip(str(x)) for x in study]) + "<br>\n"
 	return res
 
+@app.route("/study/<int:study_id>")
+def study(study_id):
+	res = "<h2> Study: " + str(study_id) + ", " + db.get_studies(study_id=study_id)[0][1] + "</h2>\n" 
+	study_participants = db.get_participants(study_id=study_id)
+	for participant in study_participants:
+		res += "<a href='/participant/%s'>" % (participant[0],)  
+		res += " : ".join([str.strip(str(x)) for x in participant])
+		res += "</a>"
+		res += "<form action='/remove_studyparticipant' method='POST'> "
+		res += "<input type='hidden' name='study_id' value='%s'> " % (study_id, )
+		res += "<input type='hidden' name='participant_id' value='%s'> " % (participant[0], )
+		res += "<input type='submit' value='Delete'> "
+		res += "</form><br>\n"
+
+	res += "<form action='/add_studyparticipant' method='POST'> "
+	res += "<p>add a participant to this study: </p>\n<select name='participant_id'>\n"
+	for participant in db.get_participants():
+		if participant not in study_participants:
+			res += "<option value='%s'>%s</option>" % (participant[0], participant[1])  
+	res += "</select>"
+	res += "<input name='study_id' style='display:none' value='%s'> " % (study_id, )
+	res += "<input type='submit' value='Add'> "
+	res += "</form> "
+	return res
+
+@app.route("/participant/<int:participant_id>")
+def participant(participant_id):
+	res = "<h2> Participant: " + str(participant_id) + "</h2>\n"
+	for image in db.get_images(participant_id):
+		res += "<img src='/%s' href='/%s'>" % (image[5], image[3])
+		res += "<p>" + " : ".join([str.strip(str(x)) for x in image]) + "</p><br>\n"
+	return res
+
+@app.route("/add_studyparticipant", methods=["POST"])
+def modify_studyparticipant():
+	study_id, participant_id = request.form['study_id'], request.form['participant_id']
+	
+	if int(study_id) % 1 == 0 and int(participant_id) % 1 == 0:
+		db.add_studyparticipant(study_id, participant_id)
+		return redirect("/study/" + study_id)
+	return "Method = " + request.method + " study_id : " + str(study_id) + " participant_id : " + str(participant_id)
+
+@app.route("/remove_studyparticipant", methods=["POST"])
+def add_studyparticipant():
+	study_id, participant_id = request.form['study_id'], request.form['participant_id']
+
+	if int(study_id) % 1 == 0 and int(participant_id) % 1 == 0:
+		db.remove_studyparticipant(study_id, participant_id)
+		return redirect("/study/" + study_id)
+
+	return "Method = " + request.method + " study_id : " + str(study_id) + " participant_id : " + str(participant_id)
+
 @app.errorhandler(500)
 def internal_server_error(error):
 	return "Error 500: " + str(error)
 
 if __name__ == "__main__":
-    app.run()
+	print "started"
+	app.run(debug=True)
+	print "started"
