322e271c8e30a2f2344c5ad42c6c8582cd46ebbb
Sven Hollowell <svenh91@gmail.com>
2016-02-11 17:35:43 +0000

    many-to-many relationship testing

--- automatic_sqlacodegen.py
+++ automatic_sqlacodegen.py
@@ -40,30 +40,41 @@ class Image(Base):
     participant = relationship(u'Participant')
 
 
+t_studyparticipants = Table(
+    'studyparticipants', metadata,
+    Column('study_id', Integer, ForeignKey(u'studies.study_id')),
+    Column('participant_id', Integer, ForeignKey(u'participants.participant_id')),
+    UniqueConstraint('study_id', 'participant_id')
+)
+
 class Participant(Base):
     __tablename__ = 'participants'
 
     participant_id = Column(Integer, primary_key=True, server_default=text("nextval('participants_participant_id_seq'::regclass)"))
-    name = Column(String(50))
+    name = Column(String(50), nullable=False)
 
-    studys = relationship(u'Study', secondary='studyparticipants')
+    studies = relationship(u'Study', secondary='studyparticipants')
 
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+            return 'Participant: %s (id=%s, studies=%s)' % (self.name, self.participant_id, [x.name for x in self.studies])
+    
 
 class Study(Base):
     __tablename__ = 'studies'
 
     study_id = Column(Integer, primary_key=True, server_default=text("nextval('studies_study_id_seq'::regclass)"))
     name = Column(String(256))
+    participants = relationship(u'Participant', secondary='studyparticipants')
 
+    def __init__(self, name):
+        self.name = name
 
-t_studyparticipants = Table(
-    'studyparticipants', metadata,
-    Column('study_id', ForeignKey(u'studies.study_id')),
-    Column('participant_id', ForeignKey(u'participants.participant_id')),
-    UniqueConstraint('study_id', 'participant_id')
-)
-
-
+    def __repr__(self):
+            return 'Study: %s (id=%s, participants=%s)' % (self.name, self.study_id, [x.name for x in self.participants])
+    
 t_useraccess = Table(
     'useraccess', metadata,
     Column('user_id', ForeignKey(u'users.user_id')),
@@ -78,31 +89,36 @@ class User(Base):
     user_id = Column(Integer, primary_key=True, server_default=text("nextval('users_user_id_seq'::regclass)"))
     username = Column(String(50))
     password = Column(String(50))
-
-    def __init__(self, username=None, password=None):
+    newprop = Column(String(10))
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
     def __repr__(self):
             return 'User: %s (id=%s, password=%s)' % (self.username, self.user_id, '*' * len(self.password))
-    def __str__(self):
-            return 'User: %s, id: %s, password: %s' % (self.username, self.user_id, '*' * len(self.password))
-
+    
 if __name__ == "__main__":
 
 
     from sqlalchemy import create_engine
     from sqlalchemy.orm import scoped_session, sessionmaker
 
-    engine = create_engine('postgres://postgres:testing@localhost:3145/linker', convert_unicode=True)
-    db_session = scoped_session(sessionmaker(autocommit=False,
-                                             autoflush=False,
+    # does not actually connect until work is done
+    engine = create_engine('postgres://postgres:testing@localhost:3145/linker', convert_unicode=True)#, echo=True)
+    db_session = scoped_session(sessionmaker(autocommit=True,
+                                             autoflush=True,
                                              bind=engine))
     Base.metadata.create_all(bind=engine)
     Base.query = db_session.query_property()
 
     u = User('Supa', 'Aiden')
     db_session.add(u)
-    db_session.commit()
     print "\n".join(map(str, User.query.all()))
+    p = Participant('hi')
+    db_session.add(p)
+    p.studies.append(Study('histudy'))
+    # db_session.update(p)
+    print "\n".join(map(str, Participant.query.all()))
+    print "\n".join(map(str, Study.query.all()))
+    # db_session.commit()
     print "done"
