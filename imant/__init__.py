5c121affc886bef810ae63de5131c92fdca693a5
sven <svenh91@gmail.com>
2016-03-18 14:55:42 +0000

    refactored out node_spawning code

--- imant/__init__.py
+++ imant/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config.from_object('config')
 
+import imant.login
 import imant.server
     
 # if __name__ == "__main__":
--- imant/run_node.py
+++ imant/run_node.py
@@ -1,50 +1,63 @@
-import subprocess, os
-print "opening node"
-import socket
-import urllib2
-import threading
-import psutil    
-import os
+from imant import app
 
-def is_node_running():
-    print "checking"
-    # print psutil.()
-    if 'node.exe' in [x for x in dir(psutil) if 'list' in x]:
-        print 'found node.exe'
-        return True
-    # timeout in seconds
-    timeout = 1
-    socket.setdefaulttimeout(timeout)
+if 1:
 
-    # this call to urllib2.urlopen now uses the default timeout
-    # we have set in the socket module
-    req = urllib2.Request('http://localhost:5001/alive')
-    try:
-        response = urllib2.urlopen(req)
-    except urllib2.URLError:
-        print "urllib2.URLError"
-        return False
-    response = response.read()
-    print 'http://localhost:5001/alive response:', str(response)
-    if response!='alive':
-        print "response not alive"
-        return False
-    return True
+    import subprocess
+    import datetime
+    @app.before_first_request
+    def before_first_request():
+        print '########### Restarted, first request @ {} ############'.format(datetime.datetime.utcnow())
+        server = subprocess.Popen(['node', 'imant/server.js'], shell=True)
 
-def run_node_server_if_not_already_running():
-    if not is_node_running():
-        server = None
+else:
+    # this has a race condition, but could be useful for querying the server in future
+    import os
+    import socket
+    import urllib2
+    import threading
+    import psutil    
+    import os
+
+    def is_node_running():
+        print "checking"
+        # print psutil.()
+        if 'node.exe' in [x for x in dir(psutil) if 'list' in x]:
+            print 'found node.exe'
+            return True
+        # timeout in seconds
+        timeout = 1
+        socket.setdefaulttimeout(timeout)
+
+        # this call to urllib2.urlopen now uses the default timeout
+        # we have set in the socket module
+        req = urllib2.Request('http://localhost:5001/alive')
         try:
-            server = subprocess.Popen(['nodemon', 'server.js'], shell=True)
-        except WindowsError:
-            print os.environ['COMSPEC']
-            subprocess.call('dir', shell=True)
-        except KeyboardInterrupt:
-            server.kill()
+            response = urllib2.urlopen(req)
+        except urllib2.URLError:
+            print "urllib2.URLError"
+            return False
+        response = response.read()
+        print 'http://localhost:5001/alive response:', str(response)
+        if response!='alive':
+            print "response not alive"
+            return False
+        return True
 
-def run_node_server():
-    t = threading.Timer(0, run_node_server_if_not_already_running)
-    t.start()
+    def run_node_server_if_not_already_running():
+        if not is_node_running():
+            server = None
+            try:
+                server = subprocess.Popen(['nodemon', 'server.js'], shell=True)
+            except WindowsError:
+                print os.environ['COMSPEC']
+                subprocess.call('dir', shell=True)
+            except KeyboardInterrupt:
+                server.kill()
+
+    def run_node_server():
+        t = threading.Timer(0, run_node_server_if_not_already_running)
+        t.start()
 
 if __name__=='__main__':
+    print "opening node"
     run_node_server()
