42da7804efcebb80c6b71ef421a4f7ab7233ca65
sven <svenh91@gmail.com>
2016-03-23 15:31:27 +0000

    refactored db.js AJAX

--- imant/static/db.js
+++ imant/static/db.js
@@ -1,103 +1,46 @@
 
 var _request_pending = false;
+function create_query(url, data, callback) {
+	if (_request_pending==true) return console.log("already waiting for a request to complete");
+	_request_pending = true;
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: data,
+		complete : function(jqXHR, textStatus) {
+			_request_pending = false;
+		  	console.log("db.js request completed: "+url+", status:"+textStatus);
+		  	if (jqXHR) console.log(jqXHR)
+			if (callback && textStatus!=="error") callback(textStatus) // if error don't reload page so we can see the response object
+		} 
+	});
+}
 var Event = {
 	// participant_id not really necessary here, but can't hurt
 	check_valid: function(participant_id, event_id, complete) {
-		_request_pending = true;
-		$.ajax({
-			type: "POST",
-			url: "/participant/"+participant_id+"/"+event_id+"/check_valid",
-			complete : function(jqXHR, textStatus) {
-				_request_pending = false;
-			  	console.log("Event.check_valid("+participant_id+","+event_id+") "+textStatus);
-			  	console.log(jqXHR)
-				if (complete) complete(textStatus)
-			} 
-		});
+		create_query("/participant/"+participant_id+"/"+event_id+"/check_valid",undefined, complete)
 	},
 	add_image: function(participant_id, event_id, image_id, complete) {
-		_request_pending = true;
-		$.ajax({
-			type: "POST",
-			url: "/participant/"+participant_id+"/"+event_id+"/"+image_id+"/add_image",
-			complete : function(jqXHR, textStatus) {
-				_request_pending = false;
-				console.log("Event.add_image("+participant_id+","+event_id+") " + textStatus);
-				console.log(jqXHR)
-				if (complete) complete(textStatus)
-			} 
-		});
+		create_query("/participant/"+participant_id+"/"+event_id+"/"+image_id+"/add_image",undefined, complete)
 	},
 	remove_image: function(participant_id, event_id, image_id, direction, include_target, complete) {
-		_request_pending = true;
-		$.ajax({
-			type: "POST",
-			url: "/participant/"+participant_id+"/"+event_id+"/"+image_id+"/remove",
-			data: {direction:direction, include_target:include_target},
-			complete : function(jqXHR, textStatus) {
-				_request_pending = false;
-				console.log("Event.remove_image("+participant_id+","+event_id+", " + direction + ", "+include_target+")" + textStatus);
-				console.log(jqXHR)
-				if (complete) complete(textStatus)
-			} 
-		});
+		create_query(
+			"/participant/"+participant_id+"/"+event_id+"/"+image_id+"/remove",
+			{direction:direction, include_target:include_target}, complete)
 	},
 	split: function(dir, participant_id, event_id, image_id, complete) {
-		_request_pending = true;
 		if (dir=="left" || dir=="right") 
-			$.ajax({
-				type: "POST",
-				url: "/participant/"+participant_id+"/"+event_id+"/"+image_id+"/split_"+dir,
-				complete : function(jqXHR, textStatus) {
-					_request_pending = false;
-					console.log("Event.split_left("+participant_id+","+event_id+") " + textStatus);
-					console.log(jqXHR)
-					if (complete) complete(textStatus)
-				} 
-			});
+			create_query("/participant/"+participant_id+"/"+event_id+"/"+image_id+"/split_"+dir,undefined, complete)
 	},
 	annotate: function(participant_id, event_id, label_id, complete) {
-		_request_pending = true;
-		$.ajax({
-			type: "POST",
-			data: {label_id:label_id},
-			url: "/participant/"+participant_id+"/"+event_id+"/annotate",
-			complete : function(jqXHR, textStatus) {
-				_request_pending = false;
-				console.log("Event.annotate("+participant_id+","+event_id+","+label_id+") " + textStatus);
-				console.log(jqXHR)
-				if (complete) complete(textStatus)
-			} 
-		});
+		create_query("/participant/"+participant_id+"/"+event_id+"/annotate",{label_id:label_id}, complete)
 	}
 }
 var Study = {
 	remove_participant: function(participant_id, study_id, complete) {
-		_request_pending = true;
-		$.ajax({
-			type: "POST",
-			url: "/remove_studyparticipant",
-			data: {participant_id: participant_id, study_id:study_id},
-			complete : function(jqXHR, textStatus) {
-				_request_pending = false;
-				console.log("/remove_studyparticipant("+participant_id+","+study_id+") " + textStatus);
-				console.log(jqXHR)
-				if (complete) complete(textStatus)
-			} 
-		});
+		create_query("/remove_studyparticipant",{participant_id: participant_id, study_id:study_id}, complete)
 	},
 	remove_user: function(user_id, study_id, complete) {
-		_request_pending = true;
-		$.ajax({
-			type: "POST",
-			url: "/user/"+user_id+"/modify_studies",
-			data: {study_id:study_id, method: "remove"},
-			complete : function(jqXHR, textStatus) {
-				_request_pending = false;
-				console.log("/user/"+user_id+"/modify_studies("+user_id+ ","+study_id+") " + textStatus);
-				console.log(jqXHR)
-				if (complete) complete(textStatus)
-			} 
-		});
+		create_query("/user/"+user_id+"/modify_studies",{study_id:study_id, method: "remove"}, complete)
 	}
 }
