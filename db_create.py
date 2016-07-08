765e25b2fa52aa07e7d4c5541acaa7123d38d933
Sven <svenh91@gmail.com>
2016-07-08 16:04:14 +0100

    don't need to drop db to create new ORM datatypes

--- db_create.py
+++ db_create.py
@@ -1,3 +1,5 @@
 from application import db
-from application import db_data
-db_data.create_data(db.session, db.engine)   
+# from application import db_data
+# db_data.create_data(db.session, db.engine)   
+# db.drop_db()
+db.create_db()
