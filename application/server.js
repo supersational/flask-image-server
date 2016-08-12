b7ea96631b98de440298a82c4f4a41750c528771
sven <svenh91@gmail.com>
2016-08-12 14:55:21 +0100

    fix length bug

--- application/server.js
+++ application/server.js
@@ -5,7 +5,7 @@ var querystring =  require('querystring')
 var url =  require('url')
 
 var secret;
-if (process.argv.length<2 || process.argv[2].length<10) {
+if (process.argv.length<3 || (process.argv.length>=3 && process.argv[2].length<10)) {
     if (process.env.NODE_SECRET_KEY) {
         console.log("NODE_SECRET_KEY set from env ");
         secret = process.env.NODE_SECRET_KEY;
